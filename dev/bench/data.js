3c242264b9339481c397937b5d5d3cfe3cf4af79
github-action-benchmark <github@users.noreply.github.com>
2024-06-19 17:11:03 +0200

    add Benchmark (customSmallerIsBetter) benchmark result for d2e2bb750d9af23a407fc7684426766cb563165c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718809585861,
+  "lastUpdate": 1718809862918,
   "repoUrl": "https://github.com/CarlWachter/hermit-rs",
   "entries": {
     "Benchmark": [
@@ -63,6 +63,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "Matmul-Speedup",
             "value": 0.8682234432234432,
+            "unit": "x"
+          },
+          {
+            "name": "Laplace 1000 Iterations",
+            "value": 82,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "carlwachter@web.de",
+            "name": "CarlWachter",
+            "username": "CarlWachter"
+          },
+          "committer": {
+            "email": "carlwachter@web.de",
+            "name": "CarlWachter",
+            "username": "CarlWachter"
+          },
+          "distinct": true,
+          "id": "d2e2bb750d9af23a407fc7684426766cb563165c",
+          "message": "feat(pages): publish to pages",
+          "timestamp": "2024-06-19T17:05:04+02:00",
+          "tree_id": "4cfa4c29efb4f91057eb89e28474c4660b48417d",
+          "url": "https://github.com/CarlWachter/hermit-rs/commit/d2e2bb750d9af23a407fc7684426766cb563165c"
+        },
+        "date": 1718809862036,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sleep-Timing",
+            "value": 100,
+            "unit": "ms"
+          },
+          {
+            "name": "Pi-Calculation-Sequential",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Pi-Calculation-Parallel",
+            "value": 2,
+            "unit": "ms"
+          },
+          {
+            "name": "Matmul-seq row-major",
+            "value": 10157000,
+            "unit": "ns"
+          },
+          {
+            "name": "Matmul-seq z-order",
+            "value": 9205000,
+            "unit": "ns"
+          },
+          {
+            "name": "Matmul-par z-order",
+            "value": 9887000,
+            "unit": "ns"
+          },
+          {
+            "name": "Matmul-par strassen",
+            "value": 9190000,
+            "unit": "ns"
+          },
+          {
+            "name": "Matmul-Speedup",
+            "value": 0.9310205320117326,
             "unit": "x"
           },
           {
